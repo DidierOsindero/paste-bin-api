dc42e9150f46ad3ad138527e396497aca50b79ef
Maria <mtens17@gmail.com>
2023-01-04 16:11:47 +0000

    Add comments database

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -6,3 +6,12 @@ CREATE TABLE pastes (
   content TEXT,
   time TIMESTAMP WITH TIME ZONE DEFAULT now()
   )
+
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  post_id INT,
+  FOREIGN KEY (post_id) REFERENCES pastes(id),
+  comment VARCHAR(140),
+  time TIMESTAMP WITH TIME ZONE DEFAULT now()
+)
