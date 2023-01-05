2eaa6252225cda8aadf914fdb2378c27da4ef9fa
Didier Osindero <didierosindero@gmail.com>
2023-01-05 12:03:29 +0000

    Update createTables

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,17 +1,17 @@
+DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS pastes;
 
 CREATE TABLE pastes (
   id SERIAL PRIMARY KEY,
   title VARCHAR(60),
-  content TEXT,
+  content TEXT NOT NULL,
   time TIMESTAMP WITH TIME ZONE DEFAULT now()
-  )
-
+  );
 
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
   paste_id INT,
   FOREIGN KEY (paste_id) REFERENCES pastes(id),
-  comment VARCHAR(140),
+  comment VARCHAR(140) NOT NULL,
   time TIMESTAMP WITH TIME ZONE DEFAULT now()
-)
+);
